37836c38f2122f592b912ee76e6429a597837a3e
Pedro A. González <pgonzalez@post.com>
2020-11-05 15:31:58 -0500

    Insert text component

---
+++ src/Text.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Text = () => {
+  return <p class="text">Click the shapes and drag the mouse to move the viewer position</p>
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Canvas } from 'react-three-fiber'
 import { softShadows, OrbitControls } from '@react-three/drei'
-
+import { Text } from './Text'
+import { Floor } from './Floor'
 import { Lights } from './Lights'
 import { ShapeCluster } from './ShapeCluster'
 import { Colors } from './utils'
@@ -10,24 +11,18 @@ import './index.css'
 
 softShadows()
 
-const Floor = () => {
-  return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -4, 0]} castShadow receiveShadow>
-      <planeBufferGeometry args={[1000, 1000]} />
-      <shadowMaterial opacity={0.4} />
-    </mesh>
-  )
-}
-
 const App = () => {
   return (
-    <Canvas colorManagement shadowMap camera={{ position: [0, 8, 15] }}>
-      <fog attach="fog" args={[Colors.fog, 8, 30]} />
-      <Lights />
-      <Floor />
-      <ShapeCluster />
-      <OrbitControls />
-    </Canvas>
+    <React.Fragment>
+      <Text />
+      <Canvas colorManagement shadowMap camera={{ position: [0, 8, 15] }}>
+        <fog attach="fog" args={[Colors.fog, 8, 30]} />
+        <Lights />
+        <Floor />
+        <ShapeCluster />
+        <OrbitControls />
+      </Canvas>
+    </React.Fragment>
   )
 }
 
